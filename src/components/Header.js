de24ecb0dff851f38325575ffd3aa0b2599027b8
chrisphine <onyangoketta@gmail.com>
2022-07-14 22:07:34 +0300

    create component Header

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from "./Header";
+ 
+function Header({name}){
+    return
+    <Header>
+        <h1>{name}</h1>
+    </Header>
+
+}
+
+export default Header
